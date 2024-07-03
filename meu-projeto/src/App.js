be004ffec012acab89b23e665dd98b82d6b9ce02
RosiestSloth <165097853+RosiestSloth@users.noreply.github.com>
2024-07-03 09:54:21 -0300

    Atualização
    Aqui foram meus primeiros passos com React, foi uma experiência incrível, gostei muito de trabalhar com HTML dentro do próprio JavaScript e conseguir demonstrar as variáveis dentro do próprio HTML.

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName';
+
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  )
+}
+
+export default App;
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,11 @@
+function Frase() {
+    var nome = 'Vinicius'
+    return(
+        <div>
+            <p>Seu nome é {nome}</p>
+            <p>Este é um componente com uma frase!</p>
+        </div>
+    )
+}
+
+export default Frase
