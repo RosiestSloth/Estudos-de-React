f8c862b4781a49cfd5f5e86e3a926c4fc5a400a5
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-01-22 11:18:10 -0300

    Update
    Atualização de design da página principal e configuração do get para dados do banco de dados serem apresentados na tela do usuário.

--- devclub_cadastro_usuarios/src/main.jsx
+++ devclub_cadastro_usuarios/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Home from './pages/Home'
+import CreateUser from './pages/Home'
+import Users from './Components/Home/Users'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <CreateUser />
+    <Users />
   </StrictMode>
 )
--- devclub_cadastro_usuarios/src/pages/Home/index.jsx
+++ devclub_cadastro_usuarios/src/pages/Home/index.jsx
@@ -1,20 +1,26 @@
-import '/src/index.css'
-
 function Home() {
 
   return (
 
-    <div className="bg-white/20 backdrop-blur-md w-full flex items-center justify-center h-120 lg:w-1/2 relative top-1/2 left-1/2 -translate-x-1/2 translate-y-1/2 rounded-xl">
-      <form action="" className='p-4 flex w-full flex-col gap-4'>
-        <h1>
+    <div className="border border-gray-200/20 backdrop-blur-md w-full max-w-150 flex items-center justify-center h-130 rounded-xl">
+      <form action="" className='p-4 flex w-full flex-col gap-4 items-center justify-center h-full'>
+        <h1 className="text-2xl font-bold text-center text-white">
           Cadastro de usuários
         </h1>
-        <input className="focus:bg-gray-300 focus:border-0 rounded-md border-1 border-gray-400 hover:bg-gray-200 active:bg-gray-300 transition p-4 w-full bg-white" name="name" type="text" />
-        <input className="focus:bg-gray-300 focus:border-0 rounded-md border-1 border-gray-400 hover:bg-gray-200 active:bg-gray-300 transition p-4 w-full bg-white" type="email" name="email" />
-        <input className="focus:bg-gray-300 focus:border-0 rounded-md border-1 border-gray-400 hover:bg-gray-200 active:bg-gray-300 transition p-4 w-full bg-white" type="date" name="birth" />
-        <input className="focus:bg-gray-300 focus:border-0 rounded-md border-1 border-gray-400 hover:bg-gray-200 active:bg-gray-300 transition p-4 w-full bg-white" type="password" name="password" />
 
-        <button type="button">Cadastrar</button>
+        <label htmlFor="name" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Nome: </label>
+        <input placeholder="Nome" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" name="name" type="text" />
+
+        <label htmlFor="email" className="text-lg px-2 font-medium text-gray-100 w-full h-3">E-Mail: </label>
+        <input placeholder="E-Mail" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="email" name="email" />
+
+        <label htmlFor="birth" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Data de Nascimento: </label>
+        <input className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="date" name="birth" />
+        
+        <label htmlFor="password" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Senha: </label>
+        <input placeholder="Senha" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="password" name="password" />
+
+        <button className="bg-purple-600 rounded-full p-3 w-1/3 cursor-pointer hover:bg-purple-700 active:bg-purple-800 transition text-gray-100 font-semibold text-lg active:scale-99" type="button">Cadastrar</button>
       </form>
     </div>
 
