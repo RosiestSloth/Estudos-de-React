9df3cea58afff0fbf62e6111e53ed7f78541c55f
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2026-01-22 15:57:42 -0300

    Update
    Atualizações de desempenho e atualizações de design

--- devclub_cadastro_usuarios/src/main.jsx
+++ devclub_cadastro_usuarios/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import CreateUser from './pages/Home'
-import Users from './Components/Home/Users'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <CreateUser />
-    <Users />
   </StrictMode>
 )
--- devclub_cadastro_usuarios/src/pages/Home/index.jsx
+++ devclub_cadastro_usuarios/src/pages/Home/index.jsx
@@ -1,30 +1,102 @@
+import { useState, useEffect, useRef } from 'react'
+import Trash from '/images/Trash.svg'
+import api from '../../services/api' 
+
 function Home() {
+    const [users, setUsers] = useState([])
 
-  return (
+    const inputName = useRef()
+    const inputEmail = useRef()
+    const inputAge = useRef()
+    const inputPass = useRef()
 
-    <div className="border border-gray-200/20 backdrop-blur-md w-full max-w-150 flex items-center justify-center h-130 rounded-xl">
-      <form action="" className='p-4 flex w-full flex-col gap-4 items-center justify-center h-full'>
-        <h1 className="text-2xl font-bold text-center text-white">
-          Cadastro de usuários
-        </h1>
+    async function getUsers() {
+        try {
+            const { data } = await api.get('/users')
+            setUsers(data)
+        } catch (error) {
+            console.error("Erro ao buscar usuários:", error)
+        }
+    }
 
-        <label htmlFor="name" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Nome: </label>
-        <input placeholder="Nome" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" name="name" type="text" />
+    async function createUsers() {
+        try {
+            await api.post('/users', {
+                name: inputName.current.value,
+                email: inputEmail.current.value,
+                age: parseInt(inputAge.current.value),
+                password: inputPass.current.value
+            })
+            
+            inputName.current.value = ''
+            inputEmail.current.value = ''
+            inputAge.current.value = ''
+            inputPass.current.value = ''
 
-        <label htmlFor="email" className="text-lg px-2 font-medium text-gray-100 w-full h-3">E-Mail: </label>
-        <input placeholder="E-Mail" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="email" name="email" />
+            getUsers()
+        } catch (error) {
+            console.error("Erro ao criar usuário:", error)
+        }
+    }
 
-        <label htmlFor="birth" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Data de Nascimento: </label>
-        <input className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="date" name="birth" />
-        
-        <label htmlFor="password" className="text-lg px-2 font-medium text-gray-100 w-full h-3">Senha: </label>
-        <input placeholder="Senha" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm transition-all focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 bg-white" type="password" name="password" />
+    async function deleteUsers(id) {
+        try {
+            await api.delete(`/users/${id}`)
+            getUsers()
+        } catch (error) {
+            console.error("Erro ao deletar usuário:", error)
+        }
+    }
 
-        <button className="bg-purple-600 rounded-full p-3 w-1/3 cursor-pointer hover:bg-purple-700 active:bg-purple-800 transition text-gray-100 font-semibold text-lg active:scale-99" type="button">Cadastrar</button>
-      </form>
-    </div>
+    useEffect(() => {
+        getUsers()
+    }, [])
 
-  )
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-10">
+            <div className="border border-gray-200/20 backdrop-blur-md w-full max-w-2xl flex flex-col items-center p-8 rounded-xl gap-6">
+                
+                <h1 className="text-3xl font-bold text-center text-white">
+                    Cadastro de usuários
+                </h1>
+
+                <form className='flex w-full flex-col gap-4 items-center justify-center'>
+                    <input placeholder="Nome" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white" type="text" ref={inputName} />
+                    <input placeholder="E-Mail" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white" type="email" ref={inputEmail} />
+                    <input placeholder="Idade" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white" type="number" ref={inputAge} />
+                    <input placeholder="Senha" className="w-full rounded-lg border border-gray-300 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm focus:border-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 bg-white" type="password" ref={inputPass} />
+
+                    <button className="bg-purple-600 rounded-full p-3 w-full max-w-xs cursor-pointer hover:bg-purple-700 active:bg-purple-800 transition text-gray-100 font-semibold text-lg active:scale-95" type="button" onClick={createUsers}>
+                        Cadastrar
+                    </button>
+                </form>
+
+                <div className="w-full bg-white rounded-xl p-4 mt-4 h-64 overflow-y-auto border-4 border-transparent
+                    scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 scrollbar-thumb-rounded-full">
+                    
+                    {users.length > 0 ? (
+                        users.map(user => (
+                            <div key={user.id} className="flex flex-row justify-between items-center mb-4 last:mb-0 bg-gray-50 p-3 rounded-lg hover:shadow-sm transition">
+                                <div>
+                                    <p className='font-semibold text-gray-700'>Nome: <span className='font-normal'>{user.name}</span></p>
+                                    <p className='font-semibold text-gray-700'>Idade: <span className='font-normal'>{user.age}</span></p>
+                                    <p className='font-semibold text-gray-700'>Email: <span className='font-normal'>{user.email}</span></p>
+                                </div>
+                                <button onClick={() => deleteUsers(user.id)} className="rounded-full bg-white text-red-500 hover:bg-red-100 p-2 shadow-sm transition hover:scale-110">
+                                    <img src={Trash} className="w-6 h-6" alt="Excluir" />
+                                </button>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="h-full flex items-center justify-center">
+                            <p className="text-gray-400 text-center">Nenhum usuário cadastrado ainda.</p>
+                        </div>
+                    )}
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default Home
